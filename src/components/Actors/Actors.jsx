7c09da01db0b2f635a98b7a4d3224599ecefe2b4
Josh Narvaez <jhnarva@gmail.com>
2022-10-19 14:52:27 -0500

    file and folder structure

---
+++ src/components/Actors/Actors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Actors = () => {
+  console.log('Actors');
+
+  return (
+    <div>Movies</div>
+  );
+};
+
+export default Actors;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  // eslint-disable-next-line no-console
+  console.log('NavBar');
+  return (
+    <div>NavBar</div>
+  );
+};
+
+export default NavBar;
