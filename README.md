2d0b46f48b59289dfc1311bb69ce91d4352faa72
Josh Narvaez <jhnarva@gmail.com>
2022-10-20 02:17:54 -0500

    sidebar is functional, pulling movies and genres from api, search is next

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Film Hub - Josh Narvaez
+
+We use redux toolkit which is a state management tool. We wrap our app in Provider which allows redux toolkit to manage the state of our entire app.
+https://redux-toolkit.js.org/
+
+we use the movie database tmdb api to get our data
+https://www.themoviedb.org/?language=en-US
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  console.log('Movies');
+  const [page, setPage] = useState(1);
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page });
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.';
 
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   );
 };
 
